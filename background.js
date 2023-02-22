45466970840d739659694793419d0b82fdd8e8c3
liu.kuan <liu.kuan@chinaebi.com>
2023-02-22 15:14:28 +0800

    add download

--- background.js
+++ background.js
@@ -1,10 +1,11 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.command === "capture") {
-      chrome.tabs.captureVisibleTab(null, {}, function(dataUrl) {
-        chrome.tabs.create({url: dataUrl});
-        sendResponse({result: "Capture successful"});
+  if (request.command === "capture") {
+    chrome.tabs.captureVisibleTab(null, {format: "png"}, function(dataUrl) {
+      var filename = "screenshot_" + Date.now() + ".png";
+      chrome.downloads.download({url: dataUrl, filename: filename}, function(downloadId) {
+        sendResponse({result: "Download started", downloadId: downloadId});
       });
-      return true;
-    }
-  });
-  
+    });
+    return true;
+  }
+});
